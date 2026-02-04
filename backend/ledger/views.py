f5c3d1fdbc3ecddbeb72d33ffb50f67015b88377
safeer85 <msafeer753@gmail.com>
2026-02-04 17:06:31 +0530

    backend deleted

--- backend/ledger/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Transaction
-from .serializers import TransactionSerializer
-
-class TransactionViewSet(viewsets.ModelViewSet):
-    serializer_class = TransactionSerializer
-
-    def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user, is_deleted=False).order_by("-occurred_at")
